286e0ece170778397f85cc6d881cc5690f945446
brownw <brownw@gmail.com>
2025-12-21 02:00:23 -0500

    Refactor player movement and add constants for player speed

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
+# Screen dimensions
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
-PLAYER_RADIUS = 20
+# Line width for drawing shapes
 LINE_WIDTH = 2
+# Player constants
 PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
+PLAYER_RADIUS = 20
